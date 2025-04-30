cdd761d0c94d33c22039245c899b7f1cdcae1185
AliArif5781 <aliairf92@gmail.com>
2025-04-30 22:20:17 +0500

    api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  // baseURL: "http://localhost:3000/",
+  baseURL: "todo-backend-production-d1a9.up.railway.app",
   withCredentials: true,
 });
