ebd24fb7b7b50143889a8019c6098829665fe901
AliArif5781 <aliairf92@gmail.com>
2025-05-03 00:10:49 +0500

    api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "http://localhost:3000/",
   baseURL: "https://todo-backend-ten.vercel.app/",
   withCredentials: true,
 });
